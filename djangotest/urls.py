afc101a70d4a4b5d04bb8fcd6a012a0066d1af92
Srishti Rajani <43403530+srishtirajani@users.noreply.github.com>
2021-04-19 20:03:30 +0530

    Add files via upload

---
+++ djangotest/urls.py
@@ -0,0 +1,39 @@
+"""djangotest URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+
+from django.conf.urls import  url
+
+from django.contrib import admin
+from . import views
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.index, name='index'),
+    url(r'^login/',views.signin,name='login'),
+    url(r'^logout/',views.signout,name='logout'),
+    url(r'^signup/',views.signup,name='signup'),
+    url(r'^profile/',views.profile,name='profile'),
+    url(r'^feedback/',views.feedback,name='feedback'),
+    url(r'^rating/',views.rating,name='rating'),
+    url(r'^changepassword/',views.changePassword,name='changePassword'),
+    url(r'^rooms/',views.rooms,name='rooms'),
+    url(r'^booking/',views.booking,name='booking'),
+    url(r'^cancel/', views.cancelBooking, name='cancelBooking'),
+    url(r'^mybookings/', views.myBookings, name='myBookings'),
+    url(r'^test/',views.test,name='test'),
+
+]
